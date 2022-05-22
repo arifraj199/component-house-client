e2079d54c0dadd435a4f4c70ecd0964f2f25db94
arifraj199 <karif9514@gmail.com>
2022-05-22 15:28:49 +0600

    simple navbar added and basic router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./Pages/About/About";
+import Blog from "./Pages/Blog/Blog";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login/Login";
+import Signup from "./Pages/Login/Signup/Signup";
+import NotFound from "./Pages/NotFound/NotFound";
+import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/blog" element={<Blog></Blog>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<Signup></Signup>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
