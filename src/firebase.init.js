7617bb64a4a69c29cae8a698b9082f414fb2c8c8
arifraj199 <karif9514@gmail.com>
2022-05-26 19:33:43 +0600

    firebase api key wrapped on env local file added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCK-DdzM7K-yxO-pFfygaxgrKRd4w3InGY",
-  authDomain: "component-house.firebaseapp.com",
-  projectId: "component-house",
-  storageBucket: "component-house.appspot.com",
-  messagingSenderId: "288939452382",
-  appId: "1:288939452382:web:0f6fa7adef73ef46075f4f"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
